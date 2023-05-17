e7364ffa883deebd3eab9d8b423a62cb533ccded
FreddieEgedal <95848391+FreddieEgedal@users.noreply.github.com>
2023-05-17 12:28:02 +0200

    javascript menu

---
+++ html/menu.js
@@ -0,0 +1,7 @@
+console.log("helloooo");
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
